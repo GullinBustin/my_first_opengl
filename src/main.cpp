02e2a698b017e28743b2d860d56f5e76c58034c5
Javier <javierportillo93@gmail.com>
2018-06-23 15:59:30 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,253 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define PI 3.14159265
+
+#include <iostream>
+
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+using namespace glm;
+
+#include "stb_image.h"
+
+#include "common/shader.hpp"
+
+#include "constants.cpp"
+
+void processInput(GLFWwindow *window);
+
+int width = 800;
+int height = 600;
+
+
+int main( void )
+{
+    // Initialise GLFW
+	if( !glfwInit() )
+	{
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		getchar();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow( width, height, "Playground", NULL, NULL);
+	if( window == NULL ){
+		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }    
+
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = LoadShaders( "resources/SimpleVertexShader.vertexshader", "resources/SimpleFragmentShader.fragmentshader" );
+
+	GLuint ModelID = glGetUniformLocation(programID, "Model");
+	GLuint ViewID = glGetUniformLocation(programID, "View");
+	GLuint ProjID = glGetUniformLocation(programID, "Projection");
+
+	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float) width / (float)height, 0.1f, 100.0f);
+	
+	// Or, for an ortho camera :
+	//glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+	
+	// Camera matrix
+	glm::mat4 View = glm::lookAt(
+		glm::vec3(-4,3,3), // Camera is at (4,3,3), in World Space
+		glm::vec3(0,0,0), // and looks at the origin
+		glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+	);
+	
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 Model = glm::translate(glm::vec3(0,0,0));
+	// Our ModelViewProjection : multiplication of our 3 matrices
+	glm::mat4 mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+	glm::mat4 Translate = glm::translate(glm::vec3(-3,0,0));
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	static const GLfloat g_vertex_buffer_data[] = { 
+		-1.0f, 0.0f, 0.0f,		1.0f,  0.0f,  0.0f,		0.0f, 0.5f,
+		 0.0f, 1.0f, 0.0f,		0.0f,  1.0f,  0.0f,		0.5f, 1.0f,
+		 0.0f,-1.0f, 0.0f,		0.0f,  0.0f,  1.0f,		0.5f, 0.0f,
+		 1.0f, 0.0f, 0.0f,		1.0f,  1.0f,  0.0f,		1.0f, 0.5f,
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertex_buffer_data), cube_vertex_buffer_data, GL_STATIC_DRAW);
+
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 2,   // first triangle
+		1, 2, 3    // second triangle
+	};
+
+	// unsigned int EBO;
+	// glGenBuffers(1, &EBO);
+	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	// glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	GLuint vertexbuffer2;
+	glGenBuffers(1, &vertexbuffer2);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertex_data), cube_vertex_data, GL_STATIC_DRAW);
+
+	unsigned int EBO2;
+	glGenBuffers(1, &EBO2);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_index_data), cube_index_data, GL_STATIC_DRAW);
+
+    // load and create a texture 
+    // -------------------------
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+    int img_width, img_height, nrChannels;
+    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
+    stbi_set_flip_vertically_on_load(true);
+	unsigned char *data = stbi_load("resources/textures/mono.jpg", &img_width, &img_height, &nrChannels, 0);
+
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img_width, img_height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+	glm::vec3 dir_luz = glm::vec3(-1,0,0);
+	GLuint LuzID = glGetUniformLocation(programID, "luzDir");
+	do{
+
+		double seconds = glfwGetTime();
+
+		Model = glm::rotate(glm::radians(360.0f/10.0f*float(seconds)), glm::vec3(0.0f,1.0f,0.0f));
+		// View = glm::lookAt(
+		// 	glm::vec3( cos( seconds * 10 * PI / 180.0 )*3 ,0,3), // Camera is at (4,3,3), in World Space
+		// 	glm::vec3(0,0,0), // and looks at the origin
+		// 	glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+		// );
+		mvp = Projection * View * Model;
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Use our shader
+		glUseProgram(programID);
+
+		glUniformMatrix4fv(ModelID, 1, GL_FALSE, &Model[0][0]);
+		glUniformMatrix4fv(ViewID, 1, GL_FALSE, &View[0][0]);
+		glUniformMatrix4fv(ProjID, 1, GL_FALSE, &Projection[0][0]);
+		// glUniform3f(LuzID, dir_luz.x, dir_luz.y, dir_luz.z);
+		glUniform3fv(LuzID, 1, &dir_luz[0]);
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			6 * sizeof(float),                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			1,                  // attribute 1. No particular reason for 1, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			6 * sizeof(float),                  // stride
+			(void*)(3 * sizeof(float))            // array buffer offset
+		);
+
+		// glEnableVertexAttribArray(2);
+		// glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		// glVertexAttribPointer(
+		// 	2,                  // attribute 1. No particular reason for 1, but must match the layout in the shader.
+		// 	2,                  // size
+		// 	GL_FLOAT,           // type
+		// 	GL_FALSE,           // normalized?
+		// 	8 * sizeof(float),                  // stride
+		// 	(void*)(6 * sizeof(float))            // array buffer offset
+		// );
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 12*3);
+//        glDrawElements(GL_TRIANGLES, 12*3, GL_UNSIGNED_INT, 0);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+		// glDisableVertexAttribArray(2);
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		
+	} // Check if the ESC key was pressed or the window was closed
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 );
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
